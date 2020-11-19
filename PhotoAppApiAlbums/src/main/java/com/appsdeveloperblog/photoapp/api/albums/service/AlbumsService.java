2dbad1486077702cbaf09f9bf875e576f7cb0333
vinay <vinaykumarby29@gmail.com>
2020-11-19 20:09:29 +0530

    Ablums microservice

---
+++ PhotoAppApiAlbums/src/main/java/com/appsdeveloperblog/photoapp/api/albums/service/AlbumsService.java
@@ -0,0 +1,9 @@
+package com.appsdeveloperblog.photoapp.api.albums.service;
+
+
+import com.appsdeveloperblog.photoapp.api.albums.data.AlbumEntity;
+import java.util.List;
+
+public interface AlbumsService {
+    List<AlbumEntity> getAlbums(String userId);
+}
---
+++ PhotoAppsApiZuulApiGateway/src/test/java/com/appsdevelopr/PhotoApps/ZuulApiGateway/PhotoAppsApiZuulApiGatewayApplicationTests.java
@@ -0,0 +1,16 @@
+package com.appsdevelopr.PhotoApps.ZuulApiGateway;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PhotoAppsApiZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
